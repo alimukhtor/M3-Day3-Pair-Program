85ecd1c12307a52ffaaa9b82074e0ccc40e0a8c5
alimukhtor <james1999007@gmail.com>
2021-10-31 21:36:51 +0100

    added some features

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ const loadUser = () =>{
                 >
                   Select
                 </button>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
               </div>
               <small class="text-muted">${element.id}</small>
             </div>
@@ -51,7 +50,7 @@ const loadUser = () =>{
 
 
 
-      `)
+      `).join('')
 
       getBody.innerHTML = allImage
       const getBtns = getBody.querySelectorAll(".hide-btn")
@@ -67,6 +66,67 @@ const loadUser = () =>{
         const currentBtn2 = getSelectedBtn[i]
         currentBtn2.onclick = function (event) {
           event.currentTarget.closest(".card").classList.add("bg-secondary");
+
+        };
+      }
+    console.log(allImage);
+  })
+}
+
+
+const loadUser1 = () =>{
+  fetch("https://api.pexels.com/v1/search?query=Tigers", {
+  "method": "GET",
+  "headers": {
+      Authorization: "563492ad6f91700001000001c5e5fe28a8174b6f8963eb819ccabd92"
+  }
+})
+  .then(resp => {
+    return resp.json()
+  })
+  .then(data => {
+    console.log(data.photos);
+    // ${element.src.small}
+    let getBody1 = document.querySelector(".album .row")
+    const allImage = data.photos.map(element =>
+      `
+
+        <div class="card col-12 col-md-4 col-lg-3 my-3 mx-3 shadow-sm">
+        <img src="${element.src.medium}" class="card-img-top">
+          <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="hide-btn btn btn-sm btn-outline-success"> Hide</button>
+                <button  type="button"  class="btn-select btn btn-sm btn-outline-success">  Select</button>
+              </div>
+              <small class="text-muted">${element.id}</small>
+            </div>
+          </div>
+        </div>
+
+
+
+
+      `).join('')
+
+      getBody1.innerHTML = allImage
+      const getBtns = getBody1.querySelectorAll(".hide-btn")
+      for(let i = 0; i < getBtns.length; i++){
+        const currentBtn1 = getBtns[i]
+        currentBtn1.onclick = function (event) {
+          event.currentTarget.closest(".card").remove();
+          // setTimeout(() => alert('Hello'), 1000);
+          setTimeout(() => alert("World"), 3000);
+
+          alert("Hello", 3000);
+        };
+      }
+      const getSelectedBtn = getBody1.querySelectorAll(".btn-select")
+      for(let i = 0; i < getSelectedBtn.length; i++){
+        const currentBtn2 = getSelectedBtn[i]
+        currentBtn2.onclick = function (event) {
+          event.currentTarget.closest(".card").classList.add("bg-secondary");
+            // setTimeout(function(){ alert("Hello") }, 5000);
 
         };
       }
