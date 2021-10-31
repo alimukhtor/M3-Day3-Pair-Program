fb1ca7ddbb41812a747f823efb8ed7fa51cd42fd
alimukhtor <james1999007@gmail.com>
2021-10-31 18:06:36 +0100

    added js code to js file and corrected mistakes

--- script.js
+++ script.js
@@ -9,70 +9,67 @@ const loadUser = () =>{
     return resp.json()
   })
   .then(data => {
-    const getRow = document.querySelector('.album .row');
-    console.log(data.photos)
-    const image = data.photos.map( element => `
-      <div class="card mb-4 shadow-sm">
-      <img src=${element.src.small}>
+    console.log(data.photos);
+    // ${element.src.small}
+    let getBody = document.querySelector(".album .row")
+    const allImage = data.photos.map(element =>
+      `
 
-        <div class="card-body">
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit
-            longer.
-          </p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button"  class="btn btn-sm btn-outline-secondary">Hide</button>
+        <div class="card col-12 col-md-4 col-lg-3 my-3 mx-3 shadow-sm">
+        <img src="${element.src.small}" class="card-img-top img-fluid w-100">
+          <div class="card-body">
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div
+              class="d-flex justify-content-between align-items-center"
+            >
+              <div class="btn-group">
+                <button
+                  type="button"
+                  class="hide-btn btn btn-sm btn-outline-success"
+                >
+                  Hide
+                </button>
+                <button
+                  type="button"
+                  class="btn-select btn btn-sm btn-outline-success"
+                >
+                  Select
+                </button>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </div>
+              <small class="text-muted">${element.id}</small>
             </div>
-            <small class="text-muted">9 mins</small>
+
+
           </div>
         </div>
-      </div>
+
+
+
 
       `)
-    getRow.innerHTML = image
 
-  })
-}
+      getBody.innerHTML = allImage
+      const getBtns = getBody.querySelectorAll(".hide-btn")
+      for(let i = 0; i < getBtns.length; i++){
+        const currentBtn1 = getBtns[i]
+        currentBtn1.onclick = function (event) {
+          event.currentTarget.closest(".card").remove();
 
-const loadUser1 = () =>{
-  fetch("https://api.pexels.com/v1/search?query=Tigers", {
-  "method": "GET",
-  "headers": {
-      Authorization: "563492ad6f91700001000001c5e5fe28a8174b6f8963eb819ccabd92"
-  }
-})
-  .then(resp => {
-    return resp.json()
-  })
-  .then(data => {
-    const getRow = document.querySelector('.album .row');
-    const getButton = document.querySelector('.btn');
+        };
+      }
+      const getSelectedBtn = getBody.querySelectorAll(".btn-select")
+      for(let i = 0; i < getSelectedBtn.length; i++){
+        const currentBtn2 = getSelectedBtn[i]
+        currentBtn2.onclick = function (event) {
+          event.currentTarget.closest(".card").classList.add("bg-secondary");
 
-
-    console.log(data.photos)
-    const image = data.photos.map( element => `  <div class="card mb-4 shadow-sm">
-    <div class="card mb-4 shadow-sm">
-      <img src=${element.src.small}>
-
-        <div class="card-body">
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit
-            longer.
-          </p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button"  class="btn btn-sm btn-outline-secondary">Hide</button>
-            </div>
-            <small class="text-muted">9 mins</small>
-          </div>
-        </div>
-      </div>
-      `)
-    getRow.innerHTML = ''
-    getRow.innerHTML += image
-
+        };
+      }
+    console.log(allImage);
   })
 }
