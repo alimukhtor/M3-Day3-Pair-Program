44c5b73a64c2708a90982cbea32b4ff9d1d0800f
alimukhtor <james1999007@gmail.com>
2021-11-01 09:25:22 +0100

    added carasoul

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const loadUser = () =>{
-  fetch("https://api.pexels.com/v1/search?query=Ocean", {
+  fetch("https://api.pexels.com/v1/search?query=ocean", {
   "method": "GET",
   "headers": {
       Authorization: "563492ad6f91700001000001c5e5fe28a8174b6f8963eb819ccabd92"
@@ -76,6 +76,50 @@ const loadUser = () =>{
       }
     console.log(allImage);
   })
+
+
+  fetch("https://api.pexels.com/v1/search?query=snake", {
+  "method": "GET",
+  "headers": {
+      Authorization: "563492ad6f91700001000001c5e5fe28a8174b6f8963eb819ccabd92"
+  }
+})
+.then(resp => {
+  return resp.json()
+})
+.then(data => {
+  console.log(data.photos);
+  let getCarasoul = document.querySelector(".ali")
+  let allCarasoul = data.photos.map(el => `
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="${el.src.large}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="${el.src.large}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="${el.src.large}" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </button>
+  </div>
+    `)
+    getCarasoul.innerHTML = allCarasoul
+})
 }
 
 
